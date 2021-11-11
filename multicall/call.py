647af3de6c23bf9c76a8c9cdd2bb93075f1ecf5c
chuwt <weitaochu@gmail.com>
2021-11-11 15:00:32 +0800

    add override param

--- multicall/call.py
+++ multicall/call.py
@@ -3,6 +3,7 @@ from typing import Optional
 from eth_utils import to_checksum_address
 from web3.auto import w3
 from multicall import Signature
+from multicall.constants import RuntimeCode
 
 
 class Call:
@@ -28,7 +29,7 @@ class Call:
     def data(self):
         return self.signature.encode_data(self.args)
 
-    def decode_output(self, output, success: Optional[bool]=None):
+    def decode_output(self, output, success: Optional[bool] = None):
         if success is None:
             apply_handler = lambda handler, value: handler(value)
         else:
@@ -54,5 +55,16 @@ class Call:
     def __call__(self, args=None):
         args = args or self.args
         calldata = self.signature.encode_data(args)
-        output = self.w3.eth.call({'to': self.target, 'data': calldata}, block_identifier=self.block_id)
+
+        output = self.w3.eth.call(
+            {'to': self.target, 'data': calldata},
+            block_identifier=self.block_id,
+            # we don't need to deploy a contract, just use override
+            # this is useful for calling the contract which is deploying ahead our contract
+            state_override={
+                self.target: {
+                    "code": RuntimeCode,
+                }
+            },
+        )
         return self.decode_output(output)
--- multicall/multicall.py
+++ multicall/multicall.py
@@ -3,11 +3,11 @@ from typing import List
 from web3.auto import w3
 
 from multicall import Call
-from multicall.constants import MULTICALL_ADDRESSES, MULTICALL2_ADDRESSES
+from multicall.constants import MULTICALL_ADDRESS
 
 
 class Multicall:
-    def __init__(self, calls: List[Call], _w3=None, block_id=None, require_success: bool=True):
+    def __init__(self, calls: List[Call], _w3=None, block_id=None, require_success: bool = True):
         self.calls = calls
         self.block_id = block_id
         self.require_success = require_success
@@ -19,14 +19,12 @@ class Multicall:
 
     def __call__(self):
         if self.require_success is True:
-            multicall_map = MULTICALL_ADDRESSES
             multicall_sig = 'aggregate((address,bytes)[])(uint256,bytes[])'
         else:
-            multicall_map = MULTICALL2_ADDRESSES
             multicall_sig = 'tryBlockAndAggregate(bool,(address,bytes)[])(uint256,uint256,(bool,bytes)[])'
 
         aggregate = Call(
-            multicall_map[self.w3.eth.chainId],
+            MULTICALL_ADDRESS,
             multicall_sig,
             returns=None,
             _w3=self.w3,
@@ -41,9 +39,8 @@ class Multicall:
             args = [self.require_success, [[call.target, call.data] for call in self.calls]]
             _, _, outputs = aggregate(args)
 
-
-        result = {}
+        result = []
         for call, (success, output) in zip(self.calls, outputs):
-            result.update(call.decode_output(output, success))
+            result.append(call.decode_output(output, success))
 
         return result
